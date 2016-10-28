246e58849483abbeadef543fe8c18f1ef06a4366
Fela Maslen <felamaslen@gmail.com>
2016-10-28 14:49:53 +0100

    User login brute force prevention

--- srv/db.py
+++ srv/db.py
@@ -6,20 +6,45 @@ import mysql.connector
 
 import db_info
 
-db_conn = mysql.connector.connect(
-    user        = db_info.DB_USERNAME,
-    password    = db_info.DB_PASSWORD,
-    database    = 'budget',
-    host        = db_info.DB_HOSTNAME,
-    port        = db_info.DB_PORT
-)
+class database:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            user        = db_info.DB_USERNAME,
+            password    = db_info.DB_PASSWORD,
+            database    = 'budget',
+            host        = db_info.DB_HOSTNAME,
+            port        = db_info.DB_PORT
+        )
 
-cursor = db_conn.cursor()
+        self.cursor = self.conn.cursor()
 
-def query(statement, data = None):
-    cursor.execute(statement, data)
+        self.needs_commit = False
 
-    return cursor
+    def close(self):
+        if self.needs_commit:
+            self.conn.commit()
 
-def last_insert_id():
-    return cursor.lastrowid
+        self.cursor.close()
+        self.conn.close()
+
+
+    def query(self, statement, data = None):
+        statement = statement.strip()
+
+        try:
+            self.cursor.execute(statement, data)
+        except AttributeError as error:
+            print "[DBFAIL] %s" % error
+
+            return False
+
+        if statement.startswith("UPDATE") or statement.startswith("INSERT"):
+            self.needs_commit = True
+
+            return True
+
+        results = list(self.cursor)
+        return results
+
+    def last_insert_id(self):
+        return self.cursor.lastrowid
--- srv/user.py
+++ srv/user.py
@@ -2,11 +2,13 @@
 Class for handling user login / logout
 """
 
-import db
+import time
 from hashlib import sha1
 
 class user:
-    def __init__(self, pin = None):
+    def __init__(self, db, pin = None):
+        self.db = db
+
         self.uid = 0
         self.name = None
 
@@ -20,30 +22,99 @@ class user:
 
         self.pin = str(pin) if not no_pin else None
 
-    def ip_check_before(self):
+    def ip_check_before(self, ip):
         """
         implements throttling to prevent brute force login attempts
         (thanks to Webster for the idea)
         """
+        num_seconds_penalty = 60 # penalty to give if IP breaches brute force limit
 
-    def ip_check_after(self):
+        num_tries = 10 # number of consecutive bad login attempts to allow
+
+        ip_check_query = self.db.query("""
+        SELECT `time`, `count` FROM ip_login_req WHERE `ip` = %s
+        """, [ip])
+
+        if not ip_check_query:
+            return None
+
+        self.ip_check_exists = False
+        self.ip_check_count = 0
+
+        for row in ip_check_query:
+            self.ip_check_exists = True
+
+            last_time = int(row[0])
+            self.ip_check_count = int(row[1])
+
+        if self.ip_check_exists:
+            breach = False
+
+            current_time = int(time.time())
+
+            if self.ip_check_count >= num_tries:
+                """ ip has done at least num_tries unsuccessful login attempts """
+                since = current_time - last_time
+
+                if since < num_seconds_penalty:
+                    """ ip has attempted another login in penalty time """
+                    breach = True
+                else:
+                    """ ip has waited for penalty time to expire """
+                    self.ip_check_count = 0
+
+            if breach:
+                """ give a penalty to ip """
+                self.db.query("""
+                UPDATE ip_login_req SET `time` = %s, `count` = %s WHERE ip = %s
+                """, [current_time, num_tries, ip])
+
+                return True
+
+        return False
+
+    def ip_check_after(self, ip):
         """
         increments the bad login counter if a bad login is attempted
         """
+        if self.uid == 0:
+            """ bad login attempted; increment or insert counter """
+            current_time = int(time.time())
 
-    def login(self):
-        if self.pin is not None:
-            info = db.query("""
-                SELECT uid, user, api_key FROM users WHERE api_key = %s
+            if self.ip_check_exists:
+                self.db.query("""
+                UPDATE ip_login_req SET `time` = %s, `count` = %s WHERE ip = %s
+                """, [current_time, self.ip_check_count + 1, ip])
+            else:
+                self.db.query("""
+                INSERT INTO ip_login_req (`ip`, `time`, `count`) VALUES (%s, %s, %s)
+                """, [ip, current_time, 1])
+
+    def login(self, ip):
+        breachedPenalty = self.ip_check_before(ip)
+
+        if breachedPenalty is None:
+            return False
+
+        if breachedPenalty is True:
+            return False
+
+        if breachedPenalty is False and self.pin is not None:
+            info = self.db.query("""
+            SELECT uid, user, api_key FROM users WHERE api_key = %s
             """, [self.password_hash()])
 
-            row = info.fetchone()
-            while row is not None:
+            if info is False:
+                return False
+
+            for row in info:
                 self.uid        = int(row[0])
                 self.name       = str(row[1])
                 self.api_key    = str(row[2])
 
-                row = info.fetchone()
+        self.ip_check_after(ip)
+
+        return True
 
     def password_hash(self):
         """
