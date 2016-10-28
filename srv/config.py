c80e9fd48aed1c6e5ef9f85aa9ed39054aa73eb3
Fela Maslen <felamaslen@gmail.com>
2016-10-29 00:49:50 +0100

    py: data/funds

--- srv/config.py
+++ srv/config.py
@@ -7,6 +7,8 @@ DEV             = True
 BASE_DIR        = os.path.dirname(os.path.realpath(__file__)) + "/.."
 SERIAL_FILE     = BASE_DIR + "/serial"
 
+FUND_SALT       = 'a963anx2'
+
 # error messages
 E_NO_PARAMS     = "Not enough parameters given"
 E_BAD_PARAMS    = "Invalid parameters given"
