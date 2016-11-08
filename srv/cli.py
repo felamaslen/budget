c69fdcb88ada874e45c6971792dea69b86307a89
Fela Maslen <felamaslen@gmail.com>
2016-11-08 20:03:09 +0000

    Working, basic cli interface

--- srv/cli.py
+++ srv/cli.py
@@ -3,11 +3,42 @@ CLI interface to budget app (backend)
 Written by Fela Maslen, 2016
 """
 
+import sys
+import urlparse
+
 import rest_api
 
-command = None # TODO: parse cli arguments
+sysargs = sys.argv[1:]
 
-api = rest_api.CommandAPI(command)
+try:
+    method, uid, task, args, form = sysargs
+except ValueError:
+    form = None
+    try:
+        method, uid, task, args = sysargs
+    except ValueError:
+        args = None
+        try:
+            method, uid, task = sysargs
+        except ValueError:
+            print("Not enough arguments given")
+            sys.exit()
 
+try:
+    method = str(method)
+
+    uid = int(uid)
+
+    task = str(task).split('/')
+
+    args = {} if args is None else urlparse.parse_qs(args)
+
+    form = {} if form is None else urlparse.parse_qs(form)
+
+except:
+    print("Bad arguments")
+    sys.exit()
+
+api = rest_api.CommandAPI(uid, method, task, args, form)
 api.print_output()
 
