8326134ed7f542fbf94cb6a45949197174e2ee2c
Fela Maslen <felamaslen@gmail.com>
2016-11-09 21:18:08 +0000

    Coloured status bar

--- cli/app/const.py
+++ cli/app/const.py
@@ -1,3 +1,5 @@
+import curses
+
 from modify import SERVER
 
 """ The api is coded in python. The .php extension is purely historical """
@@ -6,4 +8,7 @@ API_URL = SERVER + "/rest.php"
 LOGIN_FORM_TITLE = "Enter your PIN to log in: "
 LOGIN_FORM_WIDTH = 42
 LOGIN_FORM_HEIGHT = 7
+
+""" ncurses colour pairs """
+NC_COLOR_STATUS_BAR = (1, curses.COLOR_WHITE, curses.COLOR_BLUE)
 
