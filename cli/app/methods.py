770d3c501164f3fad12010fd7ebac9318bfb6e3d
Fela Maslen <felamaslen@gmail.com>
2016-11-09 21:40:54 +0000

    Method to fill window with a colour

---
+++ cli/app/methods.py
@@ -0,0 +1,20 @@
+""" extra methods to make curses programming less disgusting """
+
+import curses
+
+def window_color(x, y, w, h, color):
+    """ returns a new window filled with a background colour """
+    window = curses.newwin(h, w, y, x)
+    window.clear()
+
+    spaces = min(curses.COLS - 1, w) * ' '
+
+    for i in range(h):
+        window.addstr(i, 0, spaces, color)
+
+    return window
+
+def ellipsis(text, length):
+    """ shortens text with a [...] at the end """
+    return text if len(text) <= length else text[:5] + "[...]"
+
