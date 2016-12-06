fd986cd7be18ac86a57ce408ec341dd4989ffab3
Fela Maslen <felamaslen@gmail.com>
2016-12-06 00:34:21 +0000

    Refactor: compliant user login

--- srv/user.py
+++ srv/user.py
@@ -5,14 +5,22 @@ Class for handling user login / logout
 import time
 from hashlib import sha1
 
-from config import IP_BAN_TIME, IP_BAN_TRIES
+from srv.config import IP_BAN_TIME, IP_BAN_TRIES
 
 class User(object):
-    def __init__(self, db, pin = None):
+    """ handles user login """
+    def __init__(self, db, pin=None):
         self.dbx = db
 
         self.uid = 0
         self.name = None
+        self.api_key = None
+
+        self.ip_check = {
+            'exists': False,
+            'count': 0,
+            'expired': False
+        }
 
         no_pin = pin is None or len(pin) == 0
 
@@ -24,82 +32,79 @@ class User(object):
 
         self.pin = str(pin) if not no_pin else None
 
-    def ip_check_before(self, ip):
+    def ip_check_before(self, ip_addr):
         """
         implements throttling to prevent brute force login attempts
         (thanks to Webster for the idea)
         returns true iff ip is banned
         """
-        num_seconds_penalty = IP_BAN_TIME   # penalty to give if IP breaches brute force limit
-        num_tries           = IP_BAN_TRIES  # number of consecutive bad login attempts to allow
+        # penalty to give if IP breaches brute force limit
+        num_seconds_penalty = IP_BAN_TIME
+        # number of consecutive bad login attempts to allow
+        num_tries = IP_BAN_TRIES
 
         ip_check_query = self.dbx.query("""
         SELECT `time`, `count` FROM ip_login_req WHERE `ip` = %s
-        """, [ip])
+        """, [ip_addr])
 
         if ip_check_query is False:
             return None
 
-        self.ip_check_exists = False
-        self.ip_check_count = 0
-        self.ip_check_expired = False
+        self.ip_check['exists'] = False
+        self.ip_check['count'] = 0
+        self.ip_check['expired'] = False
 
         for row in ip_check_query:
-            self.ip_check_exists = True
+            self.ip_check['exists'] = True
 
             last_time = int(row[0])
-            self.ip_check_count = int(row[1])
+            self.ip_check['count'] = int(row[1])
 
-        if self.ip_check_exists:
-            breach = False
-
+        if self.ip_check['exists']:
             current_time = int(time.time())
-
             since = current_time - last_time
 
             if since < num_seconds_penalty:
-                """ ip has done an unsuccessful login attempt in the past penalty period """
-
-                if self.ip_check_count >= num_tries:
-                    """ ip has exceeded the fat finger allowance, so needs locking out for a penalty period """
+                # ip has done an unsuccessful login attempt in the past
+                # penalty period
+                if self.ip_check['count'] >= num_tries:
+                    # ip has exceeded the fat finger allowance, so needs
+                    # locking out for a penalty period
                     return True
 
             else:
                 """ we are beyond the penalty period, so forget about
                 all previous bad login attempts from ip """
-                self.ip_check_count = 0
-
-                self.ip_check_expired = True
+                self.ip_check['count'] = 0
+                self.ip_check['expired'] = True
 
         return False
 
-    def ip_check_after(self, ip):
-        """
-        increments the bad login counter if a bad login is attempted
-        """
+    def ip_check_after(self, ip_addr):
+        """ increments the bad login counter if a bad login is attempted """
         if self.uid == 0:
-            """ bad login attempted; increment or insert counter """
+            # bad login attempted; increment or insert counter
             current_time = int(time.time())
 
-            if self.ip_check_exists:
+            if self.ip_check['exists']:
                 self.dbx.query("""
                 UPDATE ip_login_req SET `time` = %s, `count` = %s WHERE ip = %s
-                """, [current_time, self.ip_check_count + 1, ip])
+                """, [current_time, self.ip_check['count'] + 1, ip_addr])
             else:
                 self.dbx.query("""
                 INSERT INTO ip_login_req (`ip`, `time`, `count`) VALUES (%s, %s, %s)
-                """, [ip, current_time, 1])
+                """, [ip_addr, current_time, 1])
 
-        elif self.ip_check_expired:
-            """ ip made a good login after waiting for ban time to expire; delete counter """
-
+        elif self.ip_check['expired']:
+            # ip made a good login after waiting for ban time to expire
+            # delete counter
             self.dbx.query("""
             DELETE FROM ip_login_req WHERE `ip` = %s
-            """, [ip])
+            """, [ip_addr])
 
-    def login(self, ip):
+    def login(self, ip_addr):
         """ try to log in """
-        breached_penalty = self.ip_check_before(ip)
+        breached_penalty = self.ip_check_before(ip_addr)
 
         if breached_penalty is None: # unknown error
             return None
@@ -116,11 +121,11 @@ class User(object):
                 return False
 
             for row in info:
-                self.uid        = int(row[0])
-                self.name       = str(row[1])
-                self.api_key    = str(row[2])
+                self.uid = int(row[0])
+                self.name = str(row[1])
+                self.api_key = str(row[2])
 
-        self.ip_check_after(ip)
+        self.ip_check_after(ip_addr)
 
         return True # login attempted
 
@@ -137,8 +142,8 @@ class User(object):
             return None
 
         for (uid, user) in query:
-            self.uid    = int(uid)
-            self.name   = str(user)
+            self.uid = int(uid)
+            self.name = str(user)
 
         return 1 if self.uid == 0 else 0
 
