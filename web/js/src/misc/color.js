d413f391e22fb3337704ec4dcb4d059c4d76b09e
Fela Maslen <felamaslen@gmail.com>
2017-05-13 15:59:11 +0100

    Better color keys

--- web/js/src/misc/color.js
+++ web/js/src/misc/color.js
@@ -30,3 +30,35 @@ export function getColorFromScore(color, score, negative) {
   }));
 }
 
+const colorKeyList = [
+  [1, 0, 103],
+  [255, 0, 86],
+  [158, 0, 142],
+  [14, 76, 161],
+  [0, 95, 57],
+  [149, 0, 58],
+  [255, 147, 126],
+  [0, 21, 68],
+  [107, 104, 130],
+  [0, 0, 255],
+  [0, 125, 181],
+  [106, 130, 108],
+  [0, 174, 126],
+  [194, 140, 159],
+  [190, 153, 112],
+  [0, 143, 156],
+  [95, 173, 78],
+  [255, 0, 0],
+  [255, 2, 157]
+];
+
+const colorKeyRGB = index => {
+  if (index === 0) {
+    return [0, 0, 0];
+  }
+  return colorKeyList[index % colorKeyList.length];
+};
+export const colorKey = index => {
+  return rgb(colorKeyRGB(index));
+};
+
