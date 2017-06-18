4d6a7a3452cbcdc8a5a31af0223081b7e2a4a8a4
Fela Maslen <felamaslen@gmail.com>
2017-06-18 12:27:08 +0100

    Move overview colour functions

--- web/src/js/reducers/data/overview.js
+++ web/src/js/reducers/data/overview.js
@@ -3,9 +3,10 @@
  */
 
 import { AVERAGE_MEDIAN, MONTHS_SHORT, OVERVIEW_COLUMNS } from '../../misc/const';
-import { FUTURE_INVESTMENT_RATE, COLOR_CATEGORY } from '../../misc/config';
+import { FUTURE_INVESTMENT_RATE } from '../../misc/config';
 import { yearMonthDifference } from '../../misc/date';
 import { listAverage, randnBm } from '../../misc/data.jsx';
+import { getOverviewCategoryColor, getOverviewScoreColor } from '../../misc/color';
 import { List as list, Map as map, fromJS } from 'immutable';
 
 /**
@@ -131,67 +132,6 @@ export const rProcessDataOverview = raw => {
 };
 
 /**
- * Get colours for colouring the table
- * @returns {array} list of colour codes
- */
-const getCategoryColor = () => {
-  return OVERVIEW_COLUMNS.slice(1).map(column => {
-    if (COLOR_CATEGORY[column]) {
-      return COLOR_CATEGORY[column];
-    }
-    if (column === 'Net') {
-      return [COLOR_CATEGORY.Spending, COLOR_CATEGORY.Income];
-    }
-    if (column === 'Predicted') {
-      return COLOR_CATEGORY.Balance;
-    }
-    return null;
-  });
-};
-
-/**
- * Get a colour on a scale, based on value (linear)
- * @param {integer} value: the value to score
- * @param {array} range: minimum and maximum of range
- * @param {array} median: median values in range
- * @param {array} color: color scale(s) to use
- * @returns {array} rgb values
- */
-const getScoreColor = (value, range, median, color) => {
-  if (range[0] === range[1]) {
-    return [255, 255, 255]; // white
-  }
-
-  let score;
-
-  let medianValue = median[0];
-  let cost = value;
-  let max = range[1];
-  if (value < 0) {
-    medianValue = -median[1];
-    cost *= -1;
-    max = -range[0];
-  }
-
-  if (cost > medianValue) {
-    score = 0.5 * (1 + (cost - medianValue) / (max - medianValue));
-  }
-  else {
-    score = 0.5 * cost / medianValue;
-  }
-
-  const split = color.length === 2 && (range[0] < 0 || range[1] > 0);
-  let theColor = color;
-  if (split) {
-    // score separately for positive vs. negative
-    const end = value < 0 ? 0 : 1;
-    theColor = color[end];
-  }
-
-  return theColor.map(item => Math.round(255 - (255 - item) * score));
-};
-
-/**
  * Get rows for display in the view
  * @param {List} data: processed data
  * @returns {List} rows for the view
@@ -217,7 +157,7 @@ export const rGetOverviewRows = data => {
     ];
   });
 
-  const categoryColor = getCategoryColor();
+  const categoryColor = getOverviewCategoryColor();
 
   // translate the data into table cells for display in the view
   const rows = tableData.get(0).map((monthText, key) => {
@@ -232,7 +172,7 @@ export const rGetOverviewRows = data => {
       const value = tableData.getIn([colKey, key]);
       let rgb = null;
       if (colKey > 0 && categoryColor[colKey - 1]) {
-        rgb = getScoreColor(value, valueRange[colKey - 1], median[colKey - 1], categoryColor[colKey - 1]);
+        rgb = getOverviewScoreColor(value, valueRange[colKey - 1], median[colKey - 1], categoryColor[colKey - 1]);
       }
       const editable = column === 'Balance';
 
