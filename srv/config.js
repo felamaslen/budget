1c893c96735b975d02e029c99f986cf683e0a2c5
Fela Maslen <felamaslen@gmail.com>
2017-08-17 21:24:25 +0100

    Make server config export function

--- srv/config.js
+++ srv/config.js
@@ -2,17 +2,21 @@
  * Configuration variables (mainly environment variables);
  */
 
-module.exports = {
-  debug: process.env.DEBUG === 'yes',
-  mongoUri: process.env.MONGO_URI,
-  webUri: process.env.WEB_URI,
-  userHashSalt: process.env.USER_HASH_SALT,
-  msg: {
-    unknownApiEndpoint: 'Unknown API endpoint',
-    errorServerDb: 'Unknown database error',
-    errorLoginBad: 'Bad PIN',
-    errorIpBanned: 'Banned',
-    errorNotAuthorized: 'You need to authenticate to do that'
-  }
+require('dotenv').config();
+
+module.exports = () => {
+    return {
+        debug: process.env.NODE_ENV !== 'production',
+        mongoUri: process.env.MONGO_URI,
+        webUri: process.env.WEB_URI,
+        userHashSalt: process.env.USER_HASH_SALT,
+        msg: {
+            unknownApiEndpoint: 'Unknown API endpoint',
+            errorServerDb: 'Unknown database error',
+            errorLoginBad: 'Bad PIN',
+            errorIpBanned: 'Banned',
+            errorNotAuthorized: 'You need to authenticate to do that'
+        }
+    };
 };
 
