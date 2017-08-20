25ea745c806251fea34cfbcbbac7930898ec049f
Fela Maslen <felamaslen@gmail.com>
2017-08-20 17:28:30 +0100

    Dummy client for backend development

--- web/src/js/index.jsx
+++ web/src/js/index.jsx
@@ -4,12 +4,13 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+// import App from './components/App';
 
 if (process.env.NODE_ENV !== 'test') {
-  render(
-    <App />,
-    document.getElementById('root')
-  );
+    render(
+        // <App />,
+        <h1>It works!</h1>,
+        document.getElementById('root')
+    );
 }
 
