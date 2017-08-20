87e8bea681154b0ce4600b5c080b8000efc256f7
Fela Maslen <felamaslen@gmail.com>
2017-08-20 17:32:25 +0100

    Remove old mongodb code from backend

--- srv/index.js
+++ srv/index.js
@@ -2,15 +2,14 @@
  * Launch script for the server
  */
 
-require('dotenv').config();
-const config = require('./config');
+const config = require('./config')();
 
 const serverApp = require('./server');
 
 // listen to web requests
 serverApp().then(server => {
-  if (config.debug) {
-    console.log(`App is listening on port ${server.port}!`);
-  }
+    if (config.debug) {
+        console.log(`App is listening on port ${server.port}!`);
+    }
 });
 
