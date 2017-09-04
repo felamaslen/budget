e5b3ae49b1a7c0ad10da25f0a79af868f28289de
Fela Maslen <felamaslen@gmail.com>
2017-09-04 18:19:13 +0100

    Get SQL query condition for weekly period pagination in analysis page

--- api/src/routes/data/analysis.js
+++ api/src/routes/data/analysis.js
@@ -1,3 +1,5 @@
+const common = require('../../common');
+
 function getCategoryColumn(category, grouping) {
     // get database column corresponding to "category" type
     if (category === 'bills') {
@@ -27,12 +29,40 @@ function getCategoryColumn(category, grouping) {
     return null;
 }
 
+function periodConditionWeekly(beginningOfWeek, pageIndex = 0) {
+    const referenceTime = beginningOfWeek.getTime() - 86400 * 1000 * pageIndex * 7;
+
+    const dateTimeStart = new Date(referenceTime);
+    const dateTimeEnd = new Date(referenceTime + 86400 * 1000 * 6);
+
+    const yearStart = dateTimeStart.getFullYear();
+    const monthStart = dateTimeStart.getMonth() + 1;
+    const dateStart = dateTimeStart.getDate();
+
+    const yearEnd = dateTimeEnd.getFullYear();
+    const monthEnd = dateTimeEnd.getMonth() + 1;
+    const dateEnd = dateTimeEnd.getDate();
+
+    const query = common.strip(`(
+        year > ${yearStart} OR (year = ${yearStart} AND (
+            month > ${monthStart} OR (month = ${monthStart} AND date >= ${dateStart})
+        ))
+    ) AND (
+        year < ${yearEnd} OR (year = ${yearEnd} AND (
+            month < ${monthEnd} OR (month = ${monthEnd} AND date <= ${dateEnd})
+        ))
+    )`);
+
+    return query;
+}
+
 function handler(req, res) {
     return res.end('Analysis data not done yet');
 }
 
 module.exports = {
     getCategoryColumn,
+    periodConditionWeekly,
     handler
 };
 
--- api/test/routes/data/analysis.spec.js
+++ api/test/routes/data/analysis.spec.js
@@ -21,5 +21,74 @@ describe('/data/analysis', () => {
             expect(analysis.getCategoryColumn(null, null)).to.equal(null);
         });
     });
+
+    describe('periodConditionWeekly', () => {
+        // database query for weekly period
+        it('should return the correct query', () => {
+            const exampleDates = [
+                new Date('2014-12-28'),
+                new Date('2017-09-03'),
+                new Date('2016-03-27')
+            ];
+
+            const expectedResults = [
+                `(
+                    year > 2014 OR (year = 2014 AND (
+                        month > 12 OR (month = 12 AND date >= 28)
+                    ))
+                ) AND (
+                    year < 2015 OR (year = 2015 AND (
+                        month < 1 OR (month = 1 AND date <= 3)
+                        ))
+                )`,
+                `(
+                    year > 2017 OR (year = 2017 AND (
+                        month > 9 OR (month = 9 AND date >= 3)
+                    ))
+                ) AND (
+                    year < 2017 OR (year = 2017 AND (
+                        month < 9 OR (month = 9 AND date <= 9)
+                    ))
+                )`,
+                `(
+                    year > 2016 OR (year = 2016 AND (
+                        month > 3 OR (month = 3 AND date >= 27)
+                    ))
+                ) AND (
+                    year < 2016 OR (year = 2016 AND (
+                        month < 4 OR (month = 4 AND date <= 2)
+                    ))
+                )`
+            ].map(string => string.replace(/\s+/g, ' '));
+
+            exampleDates.forEach((date, key) => {
+                expect(analysis.periodConditionWeekly(date))
+                    .to.equal(expectedResults[key]);
+            });
+        });
+
+        it('should handle pagination', () => {
+            const date = new Date('2014-12-21');
+
+            const expectedResult = index => {
+                return `(
+                    year > 2014 OR (year = 2014 AND (
+                        month > 12 OR (month = 12 AND date >= ${21 - 7 * index})
+                    ))
+                ) AND (
+                    year < 2014 OR (year = 2014 AND (
+                        month < 12 OR (month = 12 AND date <= ${27 - 7 * index})
+                    ))
+                )`.replace(/\s+/g, ' ');
+            };
+
+            new Array(3)
+                .fill(0)
+                .forEach((zero, index) => {
+                    expect(analysis.periodConditionWeekly(date, index))
+                        .to.equal(expectedResult(index));
+                });
+        });
+    });
 });
 
