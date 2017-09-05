82e64365b7039bf0d50ebccecf292ee925f703f7
fela.maslen <fela.maslen@mubaloo.com>
2017-09-05 14:11:56 +0100

    API routes skeleton structure

---
+++ api/src/routes/data/funds/index.js
@@ -0,0 +1,23 @@
+async function routeGet(req, res) {
+    return res.end('not done yet');
+}
+
+async function routePost(req, res) {
+    return res.end('not done yet');
+}
+
+async function routePut(req, res) {
+    return res.end('not done yet');
+}
+
+async function routeDelete(req, res) {
+    return res.end('not done yet');
+}
+
+module.exports = {
+    routeGet,
+    routePost,
+    routePut,
+    routeDelete
+};
+
--- api/src/routes/data/index.js
+++ api/src/routes/data/index.js
@@ -1,15 +1,42 @@
 const Database = require('../../db');
 const authMiddleware = require('../authMiddleware');
 
-const overview = require('./overview').handler;
+const config = require('../../config')();
+
+const cashflow = require('./cashflow');
 const analysis = require('./analysis');
+const analysisDeep = require('./analysis/deep');
+
+const income = require('./income');
+const bills = require('./bills');
+const funds = require('./funds');
+const food = require('./food');
+const general = require('./general');
+const social = require('./social');
+const holiday = require('./holiday');
+
+const listDataProcessor = { income, bills, funds, food, general, social, holiday };
 
 function handler(app) {
+    // all of the following routes require database and authentication middleware
     app.use('/data/*', Database.dbMiddleware, authMiddleware);
 
-    app.get('/data/overview', overview);
-    app.get('/data/analysis/:period/:groupBy/:pageIndex?', analysis.shallow);
-    app.get('/data/analysis/deep/:category/:period/:groupBy/:pageIndex?', analysis.deep);
+    // cash flow routes
+    app.get('/data/overview', cashflow.routeGet);
+    app.post('/data/balance', cashflow.routePost);
+    app.put('/data/balance', cashflow.routePut);
+
+    // analysis routes
+    app.get('/data/analysis/:period/:groupBy/:pageIndex?', analysis.routeGet);
+    app.get('/data/analysis/deep/:category/:period/:groupBy/:pageIndex?', analysisDeep.routeGet);
+
+    // list data routes
+    config.data.listCategories.forEach(category => {
+        app.get(`/data/${category}`, listDataProcessor[category].routeGet);
+        app.post(`/data/${category}`, listDataProcessor[category].routePost);
+        app.put(`/data/${category}`, listDataProcessor[category].routePut);
+        app.delete(`/data/${category}`, listDataProcessor[category].routeDelete);
+    });
 }
 
 module.exports = {
