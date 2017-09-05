5a083598dc6d6a7d98303d76bee87c8b1495526e
fela.maslen <fela.maslen@mubaloo.com>
2017-09-05 16:58:31 +0100

    Added m5 dependency

--- api/test/routes/data/funds/index.spec.js
+++ api/test/routes/data/funds/index.spec.js
@@ -9,6 +9,19 @@ const common = require('../../../test.common');
 const funds = require('../../../../src/routes/data/funds');
 
 describe('/data/funds', () => {
-    it('should do something');
+    describe('getLatestCachedValues', () => {
+        it('should return the correct query', () => {
+            const db = new common.DummyDb();
+
+            expect(funds.getLatestCachedValues(db)).to.equal([
+                'SELECT fh.hash,',
+                'GROUP_CONCAT(fc.price ORDER BY ct.time DESC) AS prices',
+                'FROM fund_cache_time ct',
+                'INNER JOIN fund_cache fc ON fc.cid = ct.cid',
+                'INNER JOIN fund_hash fh ON fh.fid = fc.fid',
+                'GROUP BY fc.fid'
+            ].join(' '));
+        });
+    });
 });
 
