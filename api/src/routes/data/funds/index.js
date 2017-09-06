10153db4cb74e4cc41e3534487663512555e03b2
fela.maslen <fela.maslen@mubaloo.com>
2017-09-06 09:29:40 +0100

    Funds data route

--- api/src/routes/data/funds/index.js
+++ api/src/routes/data/funds/index.js
@@ -1,6 +1,9 @@
+const md5 = require('md5');
+
+const config = require('../../../config')();
 const listCommon = require('../list.common');
 
-function getLatestCachedValues(db) {
+function getLatestCachedPricesQuery(db) {
     return db.query(`
     SELECT fh.hash, GROUP_CONCAT(fc.price ORDER BY ct.time DESC) AS prices
     FROM fund_cache_time ct
@@ -10,12 +13,24 @@ function getLatestCachedValues(db) {
     `);
 }
 
-function addData(row) {
-    row.t = row.t
-        ? JSON.parse(row.t)
-        : [];
+function getLatestCachedPrices(queryResult) {
+    return queryResult
+        .reduce((obj, item) => {
+            const price = item.prices
+                .split(',')
+                .slice(-1)
+                .map(value => parseFloat(value, 10));
 
-    return row;
+            obj[item.hash] = price.length
+                ? price[0]
+                : 0;
+
+            return obj;
+        }, {});
+}
+
+function fundHash(fundName) {
+    return md5(`${fundName}${config.data.fundSalt}`);
 }
 
 async function routeGet(req, res) {
@@ -24,6 +39,23 @@ async function routeGet(req, res) {
         transactions: 't',
         cost: 'c'
     };
+
+    const cachedPricesQuery = await getLatestCachedPricesQuery(req.db);
+    const cachedPrices = getLatestCachedPrices(cachedPricesQuery);
+
+    const addData = row => {
+        row.tr = row.t
+            ? JSON.parse(row.t)
+            : [];
+
+        Reflect.deleteProperty(row, 't');
+
+        const hash = fundHash(row.i);
+        row.pr = cachedPrices[hash] || 0;
+
+        return row;
+    }
+
 
     const data = await listCommon.getResults(
         req.db, req.user, new Date(), 'funds', columnMap, addData
@@ -48,7 +80,9 @@ async function routeDelete(req, res) {
 }
 
 module.exports = {
-    getLatestCachedValues,
+    getLatestCachedPricesQuery,
+    getLatestCachedPrices,
+    fundHash,
     routeGet,
     routePost,
     routePut,
--- api/test/routes/data/funds/index.spec.js
+++ api/test/routes/data/funds/index.spec.js
@@ -4,8 +4,11 @@
 
 require('dotenv').config();
 const expect = require('chai').expect;
+const md5 = require('md5');
 
 const common = require('../../../test.common');
+
+const config = require('../../../../src/config')();
 const funds = require('../../../../src/routes/data/funds');
 
 describe('/data/funds', () => {
@@ -13,7 +16,7 @@ describe('/data/funds', () => {
         it('should return the correct query', () => {
             const db = new common.DummyDb();
 
-            expect(funds.getLatestCachedValues(db)).to.equal([
+            expect(funds.getLatestCachedPricesQuery(db)).to.equal([
                 'SELECT fh.hash,',
                 'GROUP_CONCAT(fc.price ORDER BY ct.time DESC) AS prices',
                 'FROM fund_cache_time ct',
@@ -21,6 +24,14 @@ describe('/data/funds', () => {
                 'INNER JOIN fund_hash fh ON fh.fid = fc.fid',
                 'GROUP BY fc.fid'
             ].join(' '));
+        });
+    });
+
+    describe('fundHash', () => {
+        it('should return a valid hashed value', () => {
+            expect(funds.fundHash('foobar')).to.equal(
+                md5(`foobar${config.data.fundSalt}`)
+            );
         });
     });
 });
