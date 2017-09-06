4a346d0e3b044aea6788313fb0b5e7ebf5abe777
fela.maslen <fela.maslen@mubaloo.com>
2017-09-06 16:53:08 +0100

    Flag to debug MySQL queries

--- api/src/db.js
+++ api/src/db.js
@@ -65,6 +65,16 @@ class Connection {
     query(sql, ...args) {
         return new Promise((resolve, reject) => {
             this.conn.query(sql, args, (err, results) => {
+                if (process.env.SQLDEBUG === 'true') {
+                    const rawQuery = sql
+                        .replace(/\?/g, () => mysql.escape(args.shift()))
+                        .replace(/\s+/g, ' ')
+                        .replace(/^\s+/, '')
+                        .replace(/\s+$/, '');
+
+                    console.log({ rawQuery });
+                }
+
                 if (err) {
                     return reject(err);
                 }
