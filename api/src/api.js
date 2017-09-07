479cf2f1d2ece3c24870f6004b3c11bc7dba35ad
Fela Maslen <felamaslen@gmail.com>
2017-09-07 22:03:40 +0100

    Dummy search route

--- api/src/api.js
+++ api/src/api.js
@@ -6,6 +6,7 @@ const config = require('./config.js')();
 
 const user = require('./routes/user');
 const data = require('./routes/data');
+const search = require('./routes/search');
 
 function apiRouter(app) {
     // TODO: middleware to redirect old requests to the new API format
@@ -15,6 +16,9 @@ function apiRouter(app) {
 
     // data routes
     data.handler(app);
+
+    // search route
+    search.handler(app);
 
     app.use((req, res) => {
         // catch-all api endpoint
