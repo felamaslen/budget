e99fc9304bdd3cff2709ec9590a7a542e1ebfa21
Fela Maslen <felamaslen@gmail.com>
2017-09-09 11:08:01 +0100

    Colours on logger

--- api/scripts/common.js
+++ api/scripts/common.js
@@ -1,8 +1,42 @@
+const colors = require('colors');
+const dateformat = require('dateformat');
+
 const Database = require('../src/db');
 const config = require('../src/config')();
 
+function formatNow() {
+    const now = new Date();
+
+    return dateformat(now, 'isoDateTime');
+}
+
 function logger(message, key = 'MSG') {
-    console.log(`[${key}@${new Date()}]`, message);
+    let pre = `[${key}@${formatNow()}]`;
+    let post = message;
+
+    if (key === 'FATAL') {
+        pre = colors.red.underline(pre);
+    }
+    else if (key === 'ERROR') {
+        pre = colors.red(pre);
+        post = colors.red(post);
+    }
+    else if (key === 'WARN') {
+        pre = colors.yellow(pre);
+        post = colors.yellow(post);
+    }
+    else if (key === 'MSG') {
+        pre = colors.grey(pre);
+    }
+    else if (key === 'DEBUG') {
+        pre = colors.cyan(pre);
+        post = colors.cyan(post);
+    }
+    else if (key === 'SUCCESS') {
+        pre = colors.green(pre);
+    }
+
+    console.log(pre, post);
 }
 
 async function connectToDatabase() {
