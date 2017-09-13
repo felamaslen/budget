8857f6d121bc6abd1cabda77b76934b65de320c5
fela.maslen <fela.maslen@mubaloo.com>
2017-09-13 15:30:32 +0100

    Use alphavantage for stock prices (not working well)

--- web/src/misc/config.js
+++ web/src/misc/config.js
@@ -37,14 +37,16 @@ export const GRAPH_FUNDS_TENSION = 0.65;
 export const GRAPH_FUNDS_MODES = ['ROI', 'Value', 'Price'];
 export const GRAPH_FUNDS_POINT_RADIUS = 3;
 
-export const STOCK_INDICES = {
-    'LON:CTY': 'CTY',
-    'INDEXFTSE:UKX': 'FTSE 100',
-    'INDEXSP:.INX': 'S&P 500',
-    'INDEXDJX:.DJI': 'DJIA',
-    'INDEXNASDAQ:NDX': 'NASDAQ 100',
-    'INDEXNIKKEI:NI225': 'Nikkei 225'
-};
+export const STOCK_INDICES = (process.env.STOCK_INDICES || '')
+    .split(',')
+    .reduce((obj, item) => {
+        const match = item.match(/^([\w.:]+)\/([\w\s.]+)$/);
+        if (match) {
+            obj[match[1]] = match[2];
+        }
+
+        return obj;
+    }, {});
 
 export const DO_STOCKS_LIST = process.env.DO_STOCKS_LIST === 'true';
 export const STOCKS_GRAPH_RESOLUTION = 50;
