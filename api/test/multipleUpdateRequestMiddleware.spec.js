3166d1efb6f2c8dcf56c7692ad1a4e4b2be3c47c
Fela Maslen <felamaslen@gmail.com>
2017-09-13 17:53:04 +0100

    Moved multiple update request middleware to separate file

--- api/test/multipleUpdateRequestMiddleware.spec.js
+++ api/test/multipleUpdateRequestMiddleware.spec.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 
 const expect = require('chai').expect;
 
-const data = require('../../../src/routes/data');
+const middleware = require('../src/multipleUpdateRequestMiddleware');
 
-describe('Data route', () => {
+describe('Multiple update request middleware', () => {
     describe('getOverallStatusCode', () => {
         it('should handle 5xx codes', () => {
-            expect(data.getOverallStatusCode([200, 403, 503, 500].map(
+            expect(middleware.getOverallStatusCode([200, 403, 503, 500].map(
                 statusCode => {
                     return { statusCode };
                 }
             )))
                 .to.equal(500);
 
-            expect(data.getOverallStatusCode([200, 403, 503].map(
+            expect(middleware.getOverallStatusCode([200, 403, 503].map(
                 statusCode => {
                     return { statusCode };
                 }
@@ -23,21 +23,21 @@ describe('Data route', () => {
         });
 
         it('should handle 4xx codes', () => {
-            expect(data.getOverallStatusCode([200, 301, 403, 410].map(
+            expect(middleware.getOverallStatusCode([200, 301, 403, 410].map(
                 statusCode => {
                     return { statusCode };
                 }
             )))
                 .to.equal(400);
 
-            expect(data.getOverallStatusCode([200, 301, 403, 400].map(
+            expect(middleware.getOverallStatusCode([200, 301, 403, 400].map(
                 statusCode => {
                     return { statusCode };
                 }
             )))
                 .to.equal(400);
 
-            expect(data.getOverallStatusCode([200, 301, 403].map(
+            expect(middleware.getOverallStatusCode([200, 301, 403].map(
                 statusCode => {
                     return { statusCode };
                 }
@@ -46,21 +46,21 @@ describe('Data route', () => {
         });
 
         it('should handle 2xx codes', () => {
-            expect(data.getOverallStatusCode([200, 201].map(
+            expect(middleware.getOverallStatusCode([200, 201].map(
                 statusCode => {
                     return { statusCode };
                 }
             )))
                 .to.equal(200);
 
-            expect(data.getOverallStatusCode([200, 200, 200].map(
+            expect(middleware.getOverallStatusCode([200, 200, 200].map(
                 statusCode => {
                     return { statusCode };
                 }
             )))
                 .to.equal(200);
 
-            expect(data.getOverallStatusCode([201, 201].map(
+            expect(middleware.getOverallStatusCode([201, 201].map(
                 statusCode => {
                     return { statusCode };
                 }
