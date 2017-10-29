2633c549df6cbe8c57acc0da1ea8861393646272
Fela Maslen <felamaslen@gmail.com>
2017-10-29 15:55:39 +0000

    Bug fix: reused synthetic event

--- web/src/components/form-field/index.js
+++ web/src/components/form-field/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function FormFieldText({ value, onChange }) {
+    const procOnChange = evt => onChange(evt.target.value);
+
     return <div className="form-field form-field-text">
-        <input type="text" defaultValue={value} onChange={onChange} />
+        <input type="text" defaultValue={value} onChange={procOnChange} />
     </div>;
 }
 
