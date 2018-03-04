3525dc9f30169259a46a6ba14cb666627e73db93
Fela Maslen <felamaslen@gmail.com>
2018-03-04 00:24:21 +0000

    Fix search suggestions using joi for validation

--- api/src/routes/search/index.js
+++ api/src/routes/search/index.js
@@ -3,36 +3,8 @@
  */
 
 const { Router } = require('express');
-const common = require('../../common');
-
-function validateParams(table, column, searchTerm, numResults) {
-    if (numResults < 1 || numResults > 10) {
-        throw new common.ErrorBadRequest('must request between 1 and 10 results');
-    }
-
-    const tableCols = {
-        income: ['item'],
-        bills: ['item'],
-        food: ['item', 'category', 'shop'],
-        general: ['item', 'category', 'shop'],
-        holiday: ['item', 'holiday', 'shop'],
-        social: ['item', 'society', 'shop']
-    };
-
-    if (!(table in tableCols)) {
-        throw new common.ErrorBadRequest('invalid table');
-    }
-
-    if (tableCols[table].indexOf(column) === -1) {
-        throw new common.ErrorBadRequest('invalid column');
-    }
-
-    if (!searchTerm.length) {
-        throw new common.ErrorBadRequest('didn\'t provide a search term');
-    }
-
-    return true;
-}
+const joi = require('joi');
+const { searchSchema } = require('../../schema');
 
 /**
  * @swagger
@@ -86,35 +58,28 @@ function validateParams(table, column, searchTerm, numResults) {
  */
 function routeGet(config, db) {
     return async (req, res) => {
-        const table = req.params.table;
-        const column = req.params.column;
-        const searchTerm = req.params.searchTerm;
-        const numResults = parseInt(req.params.numResults || 5, 10);
+        const { error, value } = joi.validate(req.params, searchSchema);
 
-        try {
-            validateParams(table, column, searchTerm, numResults);
-        }
-        catch (err) {
-            if (err instanceof common.ErrorBadRequest) {
-                return res.status(400)
-                    .json({ errorMessage: err.message });
-            }
-
-            return res.end();
+        if (error) {
+            return res.status(400)
+                .json({ errorMessage: error.message });
         }
 
+        const { table, column, searchTerm, numResults } = value;
+
         try {
-            const result = await db.select(`${column} AS col`, `SUM(IF(${column} LIKE '${searchTerm}%', 1, 0)) AS matches`)
+            const result = await db
+                .select(db.raw(`${column}, SUM(IF(${column} LIKE '${searchTerm}%', 1, 0)) AS matches`))
                 .from(table)
                 .where(column, 'like', `%${searchTerm}%`)
                 .andWhere('uid', '=', req.user.uid)
-                .groupBy('col')
+                .groupBy(column)
                 .orderBy('matches', 'desc')
-                .orderBy('col')
+                .orderBy(column)
                 .limit(numResults);
 
             const data = {
-                list: result.map(item => String(item.col))
+                list: result.map(item => String(item[column]))
             };
 
             return res.json({ data });
--- api/src/schema/index.js
+++ api/src/schema/index.js
@@ -31,11 +31,35 @@ const analysisDeepSchema = joi.object().keys({
     category: joi.string().valid(['food', 'general', 'social', 'holiday'])
 });
 
+const searchSchema = joi.object().keys({
+    table: joi.string()
+        .valid(['income', 'bills', 'food', 'general', 'holiday', 'social'])
+        .required(),
+    column: joi.when('table', {
+        is: joi.valid(['income', 'bils']),
+        then: joi.string().valid(['item']),
+        otherwise: joi.when('table', {
+            is: joi.valid(['food', 'general']),
+            then: joi.string().valid(['item', 'category', 'shop']),
+            otherwise: joi.string()
+                .when('table', {
+                    is: joi.valid(['holiday']),
+                    then: joi.string().valid(['item', 'holiday', 'shop']),
+                    otherwise: joi.string().valid(['item', 'society', 'shop'])
+                })
+        })
+    })
+        .required(),
+    searchTerm: joi.string().min(1).required(),
+    numResults: joi.number().integer().min(1).max(10).default(5)
+});
+
 module.exports = {
     transactionSchema,
     transactionListSchema,
     balanceSchema,
     analysisSchema,
-    analysisDeepSchema
+    analysisDeepSchema,
+    searchSchema
 };
 
