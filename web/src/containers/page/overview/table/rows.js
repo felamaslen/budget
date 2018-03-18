421e3bba27bc80434b985567f16e39c04a60550a
Fela Maslen <felamaslen@gmail.com>
2018-03-18 15:59:11 +0000

    Refactored tightly-coupled overview page containers -> page, graphs

--- web/src/containers/page/overview/table/rows.js
+++ web/src/containers/page/overview/table/rows.js
@@ -1,18 +1,15 @@
-import { connect } from 'react-redux';
-
 import { List as list } from 'immutable';
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import Cells from './cells';
 
-export function OverviewTableRows({ rows, numToSkip }) {
+export default function OverviewTableRows({ rows, numToSkip, ...props }) {
     const rowsDisplay = rows
         .slice(numToSkip)
         .map((row, key) => {
             const rowKey = key + numToSkip;
 
-            return <Cells key={key} row={row} rowKey={rowKey} />;
+            return <Cells {...props} key={key} row={row} rowKey={rowKey} />;
         });
 
     return <div>{rowsDisplay}</div>;
@@ -22,10 +19,4 @@ OverviewTableRows.propTypes = {
     rows: PropTypes.instanceOf(list).isRequired,
     numToSkip: PropTypes.number.isRequired
 };
-
-const mapStateToProps = state => ({
-    rows: state.getIn(['pages', 'overview', 'rows'])
-});
-
-export default connect(mapStateToProps)(OverviewTableRows);
 
