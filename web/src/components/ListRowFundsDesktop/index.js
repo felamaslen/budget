5b95d4d9c7dfe0e3eac65914c26623f35175f74b
Fela Maslen <felamaslen@gmail.com>
2018-05-08 19:12:25 +0100

    Don't display daily gain on sold funds

--- web/src/components/ListRowFundsDesktop/index.js
+++ web/src/components/ListRowFundsDesktop/index.js
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 import GraphFundItem from '../../containers/GraphFundItem';
 import FundGainInfo from '../FundGainInfo';
 
-export default function ListRowFundsDesktop({ row, id }) {
+export default function ListRowFundsDesktop({ row, id, sold }) {
     const itemKey = PAGES.funds.cols.indexOf('item');
     const name = row.getIn(['cols', itemKey])
         .toLowerCase()
@@ -20,16 +20,17 @@ export default function ListRowFundsDesktop({ row, id }) {
         <span className={className}>
             <span className="fund-graph">
                 <div className="fund-graph-cont">
-                    <GraphFundItem name={name} id={id} />
+                    <GraphFundItem name={name} id={id} sold={sold} />
                 </div>
             </span>
-            <FundGainInfo gain={row.get('gain')} />
+            <FundGainInfo gain={row.get('gain')} sold={sold} />
         </span>
     );
 }
 
 ListRowFundsDesktop.propTypes = {
     row: PropTypes.instanceOf(map).isRequired,
-    id: PropTypes.number.isRequired
+    id: PropTypes.number.isRequired,
+    sold: PropTypes.bool
 };
 
