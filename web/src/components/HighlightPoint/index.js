582d132d8f0e2faad2637fe8341bc4d82e7ba2c7
Fela Maslen <felamaslen@gmail.com>
2018-05-09 23:35:05 +0100

    Added hover capability to overview graph

--- web/src/components/HighlightPoint/index.js
+++ web/src/components/HighlightPoint/index.js
@@ -4,32 +4,59 @@ import { FONT_GRAPH_TITLE } from '../../constants/graph';
 import { COLOR_GRAPH_TITLE, COLOR_TRANSLUCENT_DARK } from '../../constants/colors';
 import { rgba } from '../../helpers/color';
 
+function getLabelPosX(posX, width, labelWidthX) {
+    let anchorLabelX = 'middle';
+    let labelPosX = posX;
+    let rectPosX = posX - labelWidthX / 2;
+
+    if (posX >= width - labelWidthX / 2) {
+        anchorLabelX = 'end';
+        labelPosX = width;
+        rectPosX = width - labelWidthX;
+    }
+    else if (posX < labelWidthX / 2) {
+        anchorLabelX = 'start';
+        labelPosX = 0;
+        rectPosX = 0;
+    }
+
+    return { anchorLabelX, labelPosX, rectPosX };
+}
+
+function getLabelPosY(posY, height, labelHeight) {
+    let labelPosY = posY;
+    let rectPosY = labelPosY - labelHeight / 2;
+
+    if (posY >= height - labelHeight / 2) {
+        labelPosY = height - labelHeight / 2 + 2;
+        rectPosY = height - labelHeight;
+    }
+    else if (posY < labelHeight / 2) {
+        labelPosY = labelHeight / 2;
+        rectPosY = 0;
+    }
+
+    return { labelPosY, rectPosY };
+}
+
 export default function HighlightPoint({ pixX, pixY, width, height, hoverEffect, hlPoint, ...props }) {
     if (!hlPoint) {
         return null;
     }
 
+    const labelWidthX = hoverEffect.labelWidthX || 88;
+    const labelWidthY = hoverEffect.labelWidthY || 50;
+
     const [fontSize, fontFamily] = FONT_GRAPH_TITLE;
+    const labelHeight = fontSize + 2;
 
     const { valX, valY, color } = hlPoint;
 
     const posX = Math.floor(pixX(valX)) + 0.5;
     const posY = Math.floor(pixY(valY)) + 0.5;
 
-    const labelWidthX = 88;
-    const labelWidthY = 50;
-    const labelHeight = fontSize + 2;
-
-    let anchorLabelX = 'middle';
-    let labelPosX = posX;
-    if (posX >= width - labelWidthX / 2) {
-        anchorLabelX = 'end';
-        labelPosX = width;
-    }
-    else if (posX < labelWidthX / 2) {
-        anchorLabelX = 'start';
-        labelPosX = 0;
-    }
+    const { anchorLabelX, labelPosX, rectPosX } = getLabelPosX(posX, width, labelWidthX);
+    const { labelPosY, rectPosY } = getLabelPosY(posY, height, labelHeight);
 
     const labelTextX = hoverEffect.labelX(valX, props);
     const labelTextY = hoverEffect.labelY(valY, props);
@@ -48,7 +75,7 @@ export default function HighlightPoint({ pixX, pixY, width, height, hoverEffect,
     };
     const textPropsY = {
         'x': width,
-        'y': posY,
+        'y': labelPosY,
         textAnchor: 'end',
         alignmentBaseline: 'middle'
     };
@@ -56,10 +83,10 @@ export default function HighlightPoint({ pixX, pixY, width, height, hoverEffect,
     return <g className="hl-point">
         <path d={pathVertical} {...lineProps} />
         <path d={pathHorizontal} {...lineProps} />
-        <rect x={posX - labelWidthX / 2} y={height - labelHeight} width={labelWidthX} height={labelHeight}
+        <rect x={rectPosX} y={height - labelHeight} width={labelWidthX} height={labelHeight}
             fill={rgba(COLOR_TRANSLUCENT_DARK)} />
         <text {...textProps} {...textPropsX}>{labelTextX}</text>
-        <rect x={width - labelWidthY} y={posY - labelHeight / 2} width={labelWidthY} height={labelHeight}
+        <rect x={width - labelWidthY} y={rectPosY} width={labelWidthY} height={labelHeight}
             fill={rgba(COLOR_TRANSLUCENT_DARK)} />
         <text {...textProps} {...textPropsY}>{labelTextY}</text>
     </g>;
