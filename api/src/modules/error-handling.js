1bb5ac3c2483a93a3785c7ff21d72b5d6d82c754
Fela Maslen <felamaslen@gmail.com>
2019-04-26 17:44:20 +0100

    Added error handling module

---
+++ api/src/modules/error-handling.js
@@ -0,0 +1,31 @@
+function catchAsyncErrors(handler) {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res, next);
+        } catch (err) {
+            next(err);
+        }
+    };
+}
+
+function errorHandler(logger) {
+    // eslint-disable-next-line no-unused-vars
+    return (err, req, res, next) => {
+        if (err.status) {
+            res.status(err.status);
+        } else {
+            logger.warn('Unhandled API error:', err.stack);
+
+            res.status(500);
+        }
+
+        res.json({
+            err: err.message
+        });
+    };
+}
+
+module.exports = {
+    catchAsyncErrors,
+    errorHandler
+};
