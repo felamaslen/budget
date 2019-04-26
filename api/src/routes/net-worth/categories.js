ebfc6d9b68d4d9af05fc9613acb78f426a7e1b89
Fela Maslen <felamaslen@gmail.com>
2019-04-26 19:56:39 +0100

    Added modify and delete endpoints for category (net worth)

--- api/src/routes/net-worth/categories.js
+++ api/src/routes/net-worth/categories.js
@@ -17,11 +17,13 @@ const create = db => catchAsyncErrors(async (req, res) => {
     });
 });
 
+const getItem = (db, id) => db.select()
+    .from('net_worth_categories')
+    .where({ id });
+
 const read = db => catchAsyncErrors(async (req, res) => {
     if (req.params.id) {
-        const [result] = await db.select()
-            .from('net_worth_categories')
-            .where({ id: req.params.id });
+        const [result] = await getItem(db, req.params.id);
 
         if (!result) {
             throw clientError('Category not found', 404);
@@ -36,12 +38,51 @@ const read = db => catchAsyncErrors(async (req, res) => {
     return res.json(data);
 });
 
+const checkExists = async (db, id) => {
+    const [current] = await db.select('id')
+        .from('net_worth_categories')
+        .where({ id });
+
+    if (!current) {
+        throw clientError('Category not found', 404);
+    }
+
+    return current;
+};
+
+const update = db => catchAsyncErrors(async (req, res) => {
+    await checkExists(db, req.params.id);
+
+    const data = req.validBody;
+
+    await db('net_worth_categories')
+        .update(data)
+        .where({ id: req.params.id });
+
+    const [updated] = await getItem(db, req.params.id);
+
+    res.json(updated);
+});
+
+const deleteItem = db => catchAsyncErrors(async (req, res) => {
+    await checkExists(db, req.params.id);
+    await db('net_worth_categories')
+        .where({ id: req.params.id })
+        .delete();
+
+    res.status(204).end();
+});
+
 function routeCategories(config, db) {
     const router = new Router();
 
     router.post('/', validate(schemaCategory), create(db));
 
     router.get('/:id?', read(db));
+
+    router.put('/:id', validate(schemaCategory), update(db));
+
+    router.delete('/:id', deleteItem(db));
 
     return router;
 }
