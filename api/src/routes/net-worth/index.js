2edc8e042f477c9a1d0c000660c928a11ac7a704
Fela Maslen <felamaslen@gmail.com>
2019-04-27 18:59:55 +0100

    Validate against nonexistent or invalid net worth subcategory input

--- api/src/routes/net-worth/index.js
+++ api/src/routes/net-worth/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const { validate } = require('../../modules/validate');
+const { clientError, catchAsyncErrors } = require('../../modules/error-handling');
 const { schemaNetWorth } = require('../../schema/net-worth');
 const { routeCategories } = require('./categories');
 const { routeSubCategories } = require('./subcategories');
@@ -10,17 +11,77 @@ const { onRead } = require('./read');
 const { onUpdate } = require('./update');
 const { onDelete } = require('./delete');
 
+const unionSelectIds = async (db, categories, after) => {
+    if (!categories.length) {
+        return [];
+    }
+
+    const [items] = await db.raw(`
+    SELECT ids.id
+    FROM (
+        SELECT ${categories[0]} AS id
+        ${categories.slice(1)
+        .map(id => `UNION SELECT ${id}`)
+        .join('\n')}
+    ) AS ids
+    ${after}
+    `);
+
+    return items;
+};
+
+function validateCategories(db) {
+    return catchAsyncErrors(async (req, res, next) => {
+        const valuesCategories = req.validBody.values.map(({ subcategory }) => subcategory);
+        const creditLimitCategories = req.validBody.creditLimit.map(({ subcategory }) => subcategory);
+
+        const allSubCategories = valuesCategories.concat(creditLimitCategories);
+
+        const invalidIds = await unionSelectIds(db, allSubCategories, `
+        LEFT JOIN net_worth_subcategories AS nws ON nws.id = ids.id
+        WHERE nws.id IS NULL
+        `);
+
+        if (invalidIds.length) {
+            throw clientError(`Nonexistent subcategory IDs: ${
+                invalidIds.map(({ id }) => id).join(',')
+            }`, 404);
+        }
+
+        const invalidCreditCategories = await unionSelectIds(db, creditLimitCategories, `
+        LEFT JOIN net_worth_subcategories AS nws ON nws.id = ids.id
+        LEFT JOIN net_worth_categories AS nwc ON nwc.id = nws.category_id
+        WHERE nwc.id IS NULL
+            OR nws.has_credit_limit != 1
+            OR nwc.id IS NULL
+            OR nwc.type != 'liability'
+        `);
+
+        if (invalidCreditCategories.length) {
+            throw clientError(`Tried to add credit limit to non-credit subcategory: ${
+                invalidCreditCategories.map(({ id }) => id).join(',')
+            }`, 400);
+        }
+
+        if (Array.from(new Set(creditLimitCategories)).length !== creditLimitCategories.length) {
+            throw clientError('Duplicate credit limit subcategories', 400);
+        }
+
+        return next();
+    });
+}
+
 function netWorthRoute(config, db) {
     const router = new Router();
 
     router.use('/categories', routeCategories(db));
     router.use('/subcategories', routeSubCategories(db));
 
-    router.post('/', validate(schemaNetWorth), onCreate(db));
+    router.post('/', validate(schemaNetWorth), validateCategories(db), onCreate(db));
 
     router.get('/:id?', onRead(db));
 
-    router.put('/:id', validate(schemaNetWorth), onUpdate(db));
+    router.put('/:id', validate(schemaNetWorth), validateCategories(db), onUpdate(db));
 
     router.delete('/:id', onDelete(db));
 
