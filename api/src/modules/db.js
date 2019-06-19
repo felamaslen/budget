60b14bba328a5f00c773e27070b115587e0fa743
Fela Maslen <felamaslen@gmail.com>
2019-06-19 22:48:33 +0100

    Added knexfile and moved database initialisation to there

--- api/src/modules/db.js
+++ api/src/modules/db.js
@@ -1,46 +1,4 @@
-/**
- * Class and methods to access PostgreSQL database
- */
-
 const knex = require('knex');
+const knexConfig = require('../../../knexfile');
 
-function parseConnectionURI(uri = '') {
-    const matches = uri.match(/^postgres:\/\/(\w+):(\w+)@([\w-]+(\.[\w-]+)*)(:([0-9]+))?\/(\w+)$/);
-
-    if (!matches) {
-        throw new Error('invalid database string');
-    }
-
-    const [, user, password, host, , , port, database] = matches;
-
-    return {
-        user,
-        password,
-        host,
-        port,
-        database
-    };
-}
-
-function initDb(config) {
-    return knex({
-        client: 'pg',
-        connection: {
-            ...parseConnectionURI(config.postgresUri),
-            typeCast: (field, next) => {
-                if (field.type === 'TINY' && field.length === 1) {
-                    const value = field.string();
-                    if (value) {
-                        return value === '1';
-                    }
-
-                    return null;
-                }
-
-                return next();
-            }
-        }
-    });
-}
-
-module.exports = initDb;
+module.exports = () => knex(knexConfig);
