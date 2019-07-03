4fc5cfc1971e789dd354306d4c4b19c75f28b470
Fela Maslen <felamaslen@gmail.com>
2019-07-03 20:48:48 +0100

    Updated actions

---
+++ web/src/constants/actions/analysis.js
@@ -0,0 +1,5 @@
+export const ANALYSIS_OPTION_CHANGED = '@@analysis/OPTION_CHANGED';
+export const ANALYSIS_DATA_REFRESHED = '@@analysis/DATA_REFRESHED';
+export const ANALYSIS_TREE_DISPLAY_TOGGLED = '@@analysis/TREE_DISPLAY_TOGGLED';
+export const ANALYSIS_TREE_HOVERED = '@@analysis/TREE_HOVERED';
+export const ANALYSIS_BLOCK_CLICKED = '@@analysis/BLOCK_CLICKED';
---
+++ web/src/constants/actions/app.js
@@ -0,0 +1,2 @@
+export const WINDOW_RESIZED = '@@app/WINDOW_RESIZED';
+export const SETTINGS_LOADED = '@@app/SETTINGS_LOADED';
