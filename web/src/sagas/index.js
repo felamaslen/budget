3d3b016a137b0322aa8c1d8b6971a251cd96e1d2
Fela Maslen <felamaslen@gmail.com>
2019-07-06 09:33:08 +0100

    Removed obsolete sagas and tests

--- web/src/sagas/index.js
+++ web/src/sagas/index.js
@@ -1,19 +1,17 @@
 import { fork } from 'redux-saga/effects';
 
-import appSaga from './app.saga';
-import loginSaga from './login.saga';
-import dataSyncSaga from './data-sync.saga';
-import contentSaga from './content.saga';
-import editSaga from './edit.saga';
-import analysisSaga from './analysis.saga';
-import fundsSaga from './funds.saga';
+import now from '~client/sagas/now';
+import app from '~client/sagas/app';
+import login from '~client/sagas/login';
+import crud from '~client/sagas/crud';
+import analysis from '~client/sagas/analysis';
+import funds from '~client/sagas/funds';
 
 export default function *rootSaga() {
-    yield fork(appSaga);
-    yield fork(loginSaga);
-    yield fork(dataSyncSaga);
-    yield fork(contentSaga);
-    yield fork(editSaga);
-    yield fork(analysisSaga);
-    yield fork(fundsSaga);
+    yield fork(now);
+    yield fork(app);
+    yield fork(login);
+    yield fork(crud);
+    yield fork(analysis);
+    yield fork(funds);
 }
--- web/test/sagas/index.spec.js
+++ web/test/sagas/index.spec.js
@@ -2,31 +2,28 @@ import test from 'ava';
 import { testSaga } from 'redux-saga-test-plan';
 
 import rootSaga from '~client/sagas';
-import appSaga from '~client/sagas/app.saga';
-import loginSaga from '~client/sagas/login.saga';
-import dataSyncSaga from '~client/sagas/data-sync.saga';
-import contentSaga from '~client/sagas/content.saga';
-import editSaga from '~client/sagas/edit.saga';
-import analysisSaga from '~client/sagas/analysis.saga';
-import fundsSaga from '~client/sagas/funds.saga';
+import now from '~client/sagas/now';
+import app from '~client/sagas/app';
+import login from '~client/sagas/login';
+import crud from '~client/sagas/crud';
+import analysis from '~client/sagas/analysis';
+import funds from '~client/sagas/funds';
 
 test('rootSaga forking all the other sagas', t => {
     t.is(1, 1);
     testSaga(rootSaga)
         .next()
-        .fork(appSaga)
+        .fork(now)
         .next()
-        .fork(loginSaga)
+        .fork(app)
         .next()
-        .fork(dataSyncSaga)
+        .fork(login)
         .next()
-        .fork(contentSaga)
+        .fork(crud)
         .next()
-        .fork(editSaga)
+        .fork(analysis)
         .next()
-        .fork(analysisSaga)
-        .next()
-        .fork(fundsSaga)
+        .fork(funds)
         .next()
         .isDone();
 });
