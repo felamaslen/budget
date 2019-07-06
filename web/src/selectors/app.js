665ca5a1b192c2d0d1b4125461984d844c66e1df
Fela Maslen <felamaslen@gmail.com>
2019-07-06 12:40:44 +0100

    Removed obsolete selectors

--- web/src/selectors/app.js
+++ web/src/selectors/app.js
@@ -7,18 +7,3 @@ export const getNow = state => state.now;
 const getUid = state => state.login.user.uid;
 
 export const getLoggedIn = createSelector(getApiKey, getUid, (apiKey, uid) => Boolean(apiKey && uid));
-
-export const getCurrentPage = state => state.currentPage;
-
-export const getAddData = state => ({
-    fields: state.edit.addFields,
-    item: state.edit.addFieldsString
-});
-
-export const getContentParamsAnalysis = state => ({
-    periodKey: state.other.analysis.period,
-    groupingKey: state.other.analysis.grouping,
-    timeIndex: state.other.analysis.timeIndex
-});
-
-export const getLoadedStatus = (state, { page }) => page in state.pages;
--- web/test/selectors/app.spec.js
+++ web/test/selectors/app.spec.js
@@ -1,11 +1,7 @@
 import test from 'ava';
 import {
     getNow,
-    getLoggedIn,
-    getCurrentPage,
-    getAddData,
-    getContentParamsAnalysis,
-    getLoadedStatus
+    getLoggedIn
 } from '~client/selectors/app';
 
 test('getNow gets the current time from the state', t => {
@@ -39,39 +35,4 @@ test('getLoggedIn returns true iff there is an API key and a user ID in state', 
             user: { uid: 'bar' }
         }
     }), false);
-});
-
-test('getCurrentPage gets current page', t => {
-    t.is(getCurrentPage({ currentPage: 'foo' }), 'foo');
-});
-
-test('getAddData gets the fields and item', t => {
-    t.deepEqual(getAddData({
-        edit: {
-            addFields: 'foo',
-            addFieldsString: 'bar'
-        }
-    }), { fields: 'foo', item: 'bar' });
-});
-
-test('getContentParamsAnalysis gets the periodKey and groupingKey from state', t => {
-    t.deepEqual(getContentParamsAnalysis({
-        other: {
-            analysis: {
-                period: 'foo',
-                grouping: 'bar',
-                timeIndex: 'baz'
-            }
-        }
-    }), {
-        periodKey: 'foo',
-        groupingKey: 'bar',
-        timeIndex: 'baz'
-    });
-});
-
-test('getLoadedStatus returns the pagesLoaded status from state', t => {
-    t.is(getLoadedStatus({ pages: { foo: { is: 'loaded' } } }, { page: 'foo' }), true);
-    t.is(getLoadedStatus({ pages: { foo: { is: 'loaded' } } }, { page: 'bar' }), false);
-    t.is(getLoadedStatus({ pages: { foo: { is: 'loaded' } } }, { page: 'baz' }), false);
 });
