8960a5b90f524fcd6e8776b2bbe853bf64b930da
Fela Maslen <felamaslen@gmail.com>
2019-11-03 14:25:47 +0000

    Style reset

---
+++ web/src/styled/mixins.js
@@ -0,0 +1,1 @@
+export const breakpoint = (size) => `@media only screen and (min-width: ${size}px)`;
---
+++ web/src/styled/variables.js
@@ -0,0 +1,14 @@
+import { rgba } from 'polished';
+
+export const breakpoints = {
+    mobileSmall: 350,
+    mobile: 690,
+    tabletSmall: 1000,
+    tablet: 1200,
+};
+
+export const colors = {
+    'shadow-l3': rgba(0, 0, 0, 0.3),
+    'shadow-l4': rgba(0, 0, 0, 0.4),
+    'shadow-l6': rgba(0, 0, 0, 0.6),
+};
