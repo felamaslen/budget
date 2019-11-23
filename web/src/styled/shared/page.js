ad1260e90333b93eeced3054a5507cec232e07fa
Fela Maslen <felamaslen@gmail.com>
2019-11-23 14:06:14 +0000

    sc: <Content />

--- web/src/styled/shared/page.js
+++ web/src/styled/shared/page.js
@@ -1,4 +1,30 @@
 import styled from 'styled-components';
+import { breakpoints } from '~client/styled/variables';
+import { breakpoint } from '~client/styled/mixins';
+
+export const PageWrapper = styled.div`
+    display: flex;
+    flex: 1 0 0;
+    width: 100%;
+    min-height: 0;
+
+    & > div {
+        // route
+        display: flex;
+        flex-flow: column;
+        width: 100%;
+        position: relative;
+    }
+
+    ${breakpoint(breakpoints.mobile)} {
+        margin: 0 auto;
+        position: relative;
+    }
+    ${breakpoint(breakpoints.tablet)} {
+        margin: initial;
+        position: static;
+    }
+`;
 
 export const Page = styled.div`
     display: flex;
