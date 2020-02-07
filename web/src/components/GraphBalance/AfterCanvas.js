d752e2d414c3d3375e293d5c5f2898716500fcf2
Fela Maslen <felamaslen@gmail.com>
2020-02-07 18:44:00 +0000

    Don't highlight current point when toggling show all on balance graph

--- web/src/components/GraphBalance/AfterCanvas.js
+++ web/src/components/GraphBalance/AfterCanvas.js
@@ -4,13 +4,29 @@ import PropTypes from 'prop-types';
 import * as Styled from './styles';
 
 export default function AfterCanvas({ showAll, setShowAll }) {
-    const onClick = useCallback(() => setShowAll(!showAll), [
-        showAll,
-        setShowAll,
-    ]);
+    const skip = useCallback(event => {
+        event.stopPropagation();
+        event.preventDefault();
+    }, []);
+
+    const onToggle = useCallback(
+        event => {
+            skip(event);
+            setShowAll(last => !last);
+        },
+        [setShowAll, skip],
+    );
 
     return (
-        <Styled.ShowAll onClick={onClick}>
+        <Styled.ShowAll
+            onClick={onToggle}
+            onMouseMove={skip}
+            onMouseOver={skip}
+            onMouseOut={skip}
+            onTouchStart={skip}
+            onTouchMove={skip}
+            onTouchEnd={skip}
+        >
             <span>{'Show all'}</span>
             <Styled.CheckBox enabled={showAll} />
         </Styled.ShowAll>
