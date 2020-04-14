b21931e99abdfb7478571c2192a325dccc4dc352
Fela Maslen <felamaslen@gmail.com>
2020-04-14 22:13:40 +0100

    Integration test for net worth (subcategories)

--- api/src/__tests__/net-worth.integration.spec.ts
+++ api/src/__tests__/net-worth.integration.spec.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import { run } from '..';
 import db from '~api/modules/db';
+import { Category, Subcategory } from '~api/routes/net-worth/types';
 
 describe('Server - integration tests (net-worth)', () => {
   let server: Server;
@@ -37,13 +38,13 @@ describe('Server - integration tests (net-worth)', () => {
       .del();
   });
 
-  describe('categories', () => {
-    const category = {
-      type: 'asset',
-      category: 'Cash',
-      color: '#33ff11',
-    };
+  const category: Omit<Category, 'id'> = {
+    type: 'asset',
+    category: 'Cash',
+    color: '#33ff11',
+  };
 
+  describe('categories', () => {
     describe('POST /net-worth/categories', () => {
       it('should respond with the category', async () => {
         const res = await agent
@@ -169,6 +170,176 @@ describe('Server - integration tests (net-worth)', () => {
         const categories = await db('net_worth_categories').select();
 
         expect(categories).toHaveLength(0);
+      });
+    });
+  });
+
+  describe('subcategories', () => {
+    let categoryId: string;
+    let subcategory: Omit<Subcategory, 'id'>;
+
+    beforeEach(async () => {
+      [categoryId] = await db('net_worth_categories')
+        .insert(category)
+        .returning('id');
+
+      subcategory = {
+        categoryId,
+        subcategory: 'Current account',
+        hasCreditLimit: null,
+        opacity: 0.8,
+      };
+    });
+
+    describe('POST /net-worth/subcategories', () => {
+      it('should respond with the subcategory', async () => {
+        const res = await agent
+          .post('/api/v4/data/net-worth/subcategories')
+          .set('Authorization', token)
+          .send(subcategory);
+
+        expect(res.status).toBe(201);
+        expect(res.body).toEqual(expect.objectContaining(subcategory));
+        expect(res.body).toHaveProperty('id');
+      });
+
+      it('should create the subcategory in the database', async () => {
+        await agent
+          .post('/api/v4/data/net-worth/subcategories')
+          .set('Authorization', token)
+          .send(subcategory);
+
+        const subcategories = await db('net_worth_subcategories').select();
+
+        expect(subcategories).toHaveLength(1);
+        expect(subcategories[0]).toEqual(
+          expect.objectContaining({
+            category_id: categoryId,
+            subcategory: subcategory.subcategory,
+            has_credit_limit: subcategory.hasCreditLimit,
+            opacity: subcategory.opacity,
+          }),
+        );
+      });
+    });
+
+    describe('GET /net-worth/subcategories/:subcategoryId', () => {
+      let subcategoryId: string;
+
+      beforeEach(async () => {
+        [subcategoryId] = await db('net_worth_subcategories')
+          .insert({
+            category_id: categoryId,
+            subcategory: subcategory.subcategory,
+            has_credit_limit: subcategory.hasCreditLimit,
+            opacity: subcategory.opacity,
+          })
+          .returning('id');
+      });
+
+      it('should respond with the subcategory', async () => {
+        expect(subcategoryId).toBeTruthy();
+
+        const res = await agent
+          .get(`/api/v4/data/net-worth/subcategories/${subcategoryId}`)
+          .set('Authorization', token);
+
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual({
+          id: subcategoryId,
+          ...subcategory,
+        });
+      });
+    });
+
+    describe('PUT /net-worth/subcategories/:subcategoryId', () => {
+      let subcategoryId: string;
+
+      beforeEach(async () => {
+        [subcategoryId] = await db('net_worth_subcategories')
+          .insert({
+            category_id: categoryId,
+            subcategory: subcategory.subcategory,
+            has_credit_limit: subcategory.hasCreditLimit,
+            opacity: subcategory.opacity,
+          })
+          .returning('id');
+      });
+
+      it('should respond with the updated subcategory', async () => {
+        expect(subcategoryId).toBeTruthy();
+
+        const res = await agent
+          .put(`/api/v4/data/net-worth/subcategories/${subcategoryId}`)
+          .set('Authorization', token)
+          .send({
+            ...subcategory,
+            subcategory: 'Savings account',
+          });
+
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual({
+          id: subcategoryId,
+          ...subcategory,
+          subcategory: 'Savings account',
+        });
+      });
+
+      it('should update the subcategory in the database', async () => {
+        await agent
+          .put(`/api/v4/data/net-worth/subcategories/${subcategoryId}`)
+          .set('Authorization', token)
+          .send({
+            ...subcategory,
+            subcategory: 'Savings account',
+          });
+
+        const subcategories = await db('net_worth_subcategories').select();
+
+        expect(subcategories).toHaveLength(1);
+        expect(subcategories[0]).toEqual(
+          expect.objectContaining({
+            category_id: categoryId,
+            subcategory: 'Savings account',
+            has_credit_limit: subcategory.hasCreditLimit,
+            opacity: subcategory.opacity,
+          }),
+        );
+      });
+    });
+
+    describe('DELETE /net-worth/categories/:categoryId', () => {
+      let subcategoryId: string;
+
+      beforeEach(async () => {
+        [subcategoryId] = await db('net_worth_subcategories')
+          .insert({
+            category_id: categoryId,
+            subcategory: subcategory.subcategory,
+            has_credit_limit: subcategory.hasCreditLimit,
+            opacity: subcategory.opacity,
+          })
+          .returning('id');
+      });
+
+      it('should respond with the 204 no content', async () => {
+        expect(subcategoryId).toBeTruthy();
+
+        const res = await agent
+          .delete(`/api/v4/data/net-worth/subcategories/${subcategoryId}`)
+          .set('Authorization', token);
+
+        expect(res.status).toBe(204);
+      });
+
+      it('should delete the subcategory in the database', async () => {
+        await agent
+          .delete(`/api/v4/data/net-worth/subcategories/${subcategoryId}`)
+          .set('Authorization', token);
+
+        const subcategories = await db('net_worth_subcategories').select();
+
+        expect(subcategories).toHaveLength(0);
       });
     });
   });
---
+++ api/src/routes/net-worth/types.ts
@@ -0,0 +1,14 @@
+export type Category = {
+  id: string;
+  type: 'asset' | 'liability';
+  category: string;
+  color: string;
+};
+
+export type Subcategory = {
+  id: string;
+  categoryId: Category['id'];
+  subcategory: string;
+  hasCreditLimit: boolean | null;
+  opacity: number;
+};
