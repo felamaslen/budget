dcac548a8710117808a05d67d6820203a9cb6bda
Fela Maslen <felamaslen@gmail.com>
2020-04-16 15:46:16 +0100

    Fixed jest alias for client code

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
-  roots: ['./api/src'],
+  roots: ['./api/src', './web/src'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.json',
@@ -12,7 +12,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
     '~api/(.*)': '<rootDir>/api/src/$1',
-    '~web/(.*)': '<rootDir>/web/src/$1',
+    '~client/(.*)': '<rootDir>/web/src/$1',
   },
   globalSetup: '<rootDir>/api/src/test-setup.ts',
   globalTeardown: '<rootDir>/api/src/test-teardown.ts',
