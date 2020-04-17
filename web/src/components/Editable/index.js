7948ca75d0084d5f03fe3bbcb7275e23cce8b023
Fela Maslen <felamaslen@gmail.com>
2020-04-17 18:29:30 +0100

    Updated string property to inline in <Editable />

--- web/src/components/Editable/index.js
+++ web/src/components/Editable/index.js
@@ -10,73 +10,68 @@ import FormFieldTransactions from '~client/components/FormField/transactions';
 import * as Styled from './styles';
 
 function EditableField({ id, item, onChange, ...rest }) {
-    const onChangeCallback = useCallback(value => onChange(item, value), [onChange, item]);
+  const onChangeCallback = useCallback(value => onChange(item, value), [onChange, item]);
 
-    const props = { ...rest, onChange: onChangeCallback };
+  const props = { ...rest, onChange: onChangeCallback };
 
-    if (item === 'date') {
-        return <FormFieldDate string label={`date-input-${id}`} {...props} />;
-    }
-    if (item === 'cost') {
-        return <FormFieldCost string label={`cost-input-${id}`} {...props} />;
-    }
-    if (item === 'transactions') {
-        return <FormFieldTransactions create {...props} />;
-    }
+  if (item === 'date') {
+    return <FormFieldDate inline label={`date-input-${id}`} {...props} />;
+  }
+  if (item === 'cost') {
+    return <FormFieldCost inline label={`cost-input-${id}`} {...props} />;
+  }
+  if (item === 'transactions') {
+    return <FormFieldTransactions create {...props} />;
+  }
 
-    return <FormFieldText string label={`${item}-input-${id}`} {...props} />;
+  return <FormFieldText inline label={`${item}-input-${id}`} {...props} />;
 }
 
 EditableField.propTypes = {
-    id: PropTypes.string.isRequired,
-    item: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  item: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default function Editable({ page, id, active, item, onSuggestion, ...props }) {
-    const [typed, onType] = useState('');
-    useEffect(() => {
-        if (!active) {
-            onType('');
-        }
-    }, [active]);
+  const [typed, onType] = useState('');
+  useEffect(() => {
+    if (!active) {
+      onType('');
+    }
+  }, [active]);
 
-    const showSuggestions = Boolean(
-        active &&
-            typed.length &&
-            PAGES_SUGGESTIONS.includes(page) &&
-            PAGES[page].suggestions &&
-            PAGES[page].suggestions.includes(item),
-    );
+  const showSuggestions = Boolean(
+    active &&
+      typed.length &&
+      PAGES_SUGGESTIONS.includes(page) &&
+      PAGES[page].suggestions &&
+      PAGES[page].suggestions.includes(item),
+  );
 
-    return (
-        <Styled.Editable active={active} item={item}>
-            <EditableField id={id} active={active} item={item} onType={onType} {...props} />
-            {showSuggestions && (
-                <SuggestionsList
-                    page={page}
-                    column={item}
-                    search={typed}
-                    onConfirm={onSuggestion}
-                />
-            )}
-        </Styled.Editable>
-    );
+  return (
+    <Styled.Editable active={active} item={item}>
+      <EditableField id={id} active={active} item={item} onType={onType} {...props} />
+      {showSuggestions && (
+        <SuggestionsList page={page} column={item} search={typed} onConfirm={onSuggestion} />
+      )}
+    </Styled.Editable>
+  );
 }
 
 Editable.propTypes = {
-    page: PropTypes.string.isRequired,
-    id: PropTypes.string,
-    active: PropTypes.bool,
-    item: PropTypes.string.isRequired,
-    onSuggestion: PropTypes.func,
-    onChange: PropTypes.func,
+  page: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  active: PropTypes.bool,
+  item: PropTypes.string.isRequired,
+  onSuggestion: PropTypes.func,
+  onChange: PropTypes.func,
 };
 
 Editable.defaultProps = {
-    active: false,
-    id: '',
-    value: '',
-    onSuggestion: () => null,
-    onChange: () => null,
+  active: false,
+  id: '',
+  value: '',
+  onSuggestion: () => null,
+  onChange: () => null,
 };
