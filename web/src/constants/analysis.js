427aa4bf5a8f1a9588c13f6c10e379d92a029cb3
Fela Maslen <felamaslen@gmail.com>
2020-05-02 18:44:20 +0100

    Typed analysis constants

--- web/src/constants/analysis.js
+++
@@ -1,4 +0,0 @@
-export const ANALYSIS_PERIODS = ['year', 'month', 'week'];
-export const ANALYSIS_GROUPINGS = ['category', 'shop'];
-export const ANALYSIS_VIEW_WIDTH = 500;
-export const ANALYSIS_VIEW_HEIGHT = 500;
