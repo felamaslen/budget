7caba11f83d78539b0083c32dd863bace94648b3
Fela Maslen <felamaslen@gmail.com>
2020-05-27 00:12:23 +0100

    fix(fund-scraper): rounding error on sold funds filter

--- api/src/scripts/scrape-funds/process.spec.ts
+++ api/src/scripts/scrape-funds/process.spec.ts
@@ -1,6 +1,6 @@
-import db from '~api/modules/db';
-import { Broker } from './types';
 import { getBroker, getFunds } from './process';
+import { Broker } from './types';
+import db from '~api/modules/db';
 
 describe('getBroker', () => {
   const TEST_FUND_NAMES = [
@@ -9,12 +9,14 @@ describe('getBroker', () => {
   ];
 
   it('returns HL for valid fund names', () => {
+    expect.assertions(2);
     expect(getBroker(TEST_FUND_NAMES[0])).toBe(Broker.HL);
     expect(getBroker(TEST_FUND_NAMES[1])).toBe(Broker.HL);
   });
 
   it('throws an error for invalid fund names', () => {
-    expect(() => getBroker('foo')).toThrowError(/: foo/);
+    expect.assertions(1);
+    expect(() => getBroker('foo')).toThrowErrorMatchingInlineSnapshot(`"Invalid fund name: foo"`);
   });
 });
 
@@ -24,12 +26,8 @@ describe('getFunds', () => {
   let uid2: string;
 
   beforeAll(async () => {
-    await db('users')
-      .select()
-      .del();
-    await db('funds')
-      .select()
-      .del();
+    await db('users').select().del();
+    await db('funds').select().del();
 
     [uid1] = await db('users')
       .insert({ name: 'test-user-1', pin_hash: 'some-pin-hash' })
@@ -63,21 +61,23 @@ describe('getFunds', () => {
       { fund_id: fundIds[0], date: '2016-08-24', units: 89.095, cost: 100000 },
       { fund_id: fundIds[0], date: '2016-09-19', units: 894.134, cost: 100000 },
       { fund_id: fundIds[0], date: '2017-04-27', units: -883.229, cost: -230000 },
-      { fund_id: fundIds[1], date: '2016-11-10', units: 30, cost: 193 },
-      { fund_id: fundIds[1], date: '2017-04-03', units: -30, cost: -203 },
+      { fund_id: fundIds[1], date: '2016-09-19', units: 1678.42, cost: 200000 },
+      { fund_id: fundIds[1], date: '2017-02-14', units: 846.38, cost: 100000 },
+      { fund_id: fundIds[1], date: '2017-10-25', units: 817, cost: 100000 },
+      { fund_id: fundIds[1], date: '2017-03-14', units: 1217.43, cost: 150000 },
+      { fund_id: fundIds[1], date: '2017-09-24', units: -4559.23, cost: -559520 },
+
       { fund_id: fundIds[2], date: '2016-08-07', units: 1032.19, cost: 560321 },
       { fund_id: fundIds[3], date: '2018-03-01', units: 14, cost: 243032 },
     ]);
   });
 
   afterAll(async () => {
-    await db('users')
-      .select()
-      .where({ name: 'test-user-2' })
-      .del();
+    await db('users').select().where({ name: 'test-user-2' }).del();
   });
 
   it('should get the list of funds (separated by uid) with total units and costs', async () => {
+    expect.assertions(3);
     const result = await getFunds();
 
     expect(result).toHaveLength(3);
@@ -85,7 +85,7 @@ describe('getFunds', () => {
     const user1 = result.filter(({ uid: userId }) => userId === uid1);
     const user2 = result.filter(({ uid: userId }) => userId === uid2);
 
-    expect(user1).toEqual([
+    expect(user1).toStrictEqual([
       expect.objectContaining({
         uid: uid1,
         name: 'City of London Investment Trust ORD 25p (share)',
@@ -95,7 +95,7 @@ describe('getFunds', () => {
       }),
     ]);
 
-    expect(user2).toEqual([
+    expect(user2).toStrictEqual([
       expect.objectContaining({
         uid: uid2,
         name: 'Apple Inc Com Stk NPV (share)',
@@ -114,6 +114,7 @@ describe('getFunds', () => {
   });
 
   it('should filter out funds with no held units', async () => {
+    expect.assertions(1);
     const result = await getFunds();
 
     // this fund has been completely sold
@@ -121,6 +122,7 @@ describe('getFunds', () => {
   });
 
   it('should throw an error if one of the funds has an invalid name', async () => {
+    expect.assertions(1);
     const [badFundId] = await db('funds')
       .insert([
         {
@@ -135,10 +137,10 @@ describe('getFunds', () => {
       { fund_id: badFundId, date: '2017-02-14', units: 846.38, cost: 100000 },
     ]);
 
-    await expect(getFunds()).rejects.toThrowError(/: Some invalid fund name/);
+    await expect(getFunds()).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Invalid fund name: Some invalid fund name"`,
+    );
 
-    await db('funds')
-      .where('id', badFundId)
-      .del();
+    await db('funds').where('id', badFundId).del();
   });
 });
--- api/src/scripts/scrape-funds/process.ts
+++ api/src/scripts/scrape-funds/process.ts
@@ -1,15 +1,15 @@
-import { QueryBuilder } from 'knex';
 import groupBy from 'lodash/groupBy';
+import { sql } from 'slonik';
 
-import db from '~api/modules/db';
-import config from '~api/config';
-import logger from '~api/modules/logger';
-import { fundHash } from '~api/routes/data/funds/common';
-import { CLIOptions, Broker, Fund } from './types';
 import { getCurrencyPrices } from './currencies';
-import { getFundUrl, downloadMultipleUrls } from './scrape';
 import { scrapeFundHoldings } from './holdings';
 import { scrapeFundPrices } from './prices';
+import { getFundUrl, downloadMultipleUrls } from './scrape';
+import { CLIOptions, Broker, Fund } from './types';
+import config from '~api/config';
+import { withSlonik } from '~api/modules/db';
+import logger from '~api/modules/logger';
+import { fundHash } from '~api/routes/data/funds/common';
 
 export function getBroker(name: string): Broker {
   if (config.data.funds.scraper.regex.test(name)) {
@@ -20,49 +20,46 @@ export function getBroker(name: string): Broker {
   throw new Error(`Invalid fund name: ${name}`);
 }
 
-export async function getFunds(): Promise<Fund[]> {
-  const rows = await db
-    .select<
-      {
-        uid: string;
-        item: string;
-        units: number;
-        cost: number;
-      }[]
-    >('uid', 'item', 'units', 'cost')
-    .from(
-      (qb1: QueryBuilder): QueryBuilder =>
-        qb1
-          .select(
-            'f.uid',
-            'f.item',
-            db.raw('SUM(ft.units)::float AS units'),
-            db.raw('SUM(ft.cost)::float AS cost'),
-          )
-          .from('funds as f')
-          .innerJoin('funds_transactions as ft', 'ft.fund_id', 'f.id')
-          .groupBy('f.uid')
-          .groupBy('f.item')
-          .as('r'),
-    )
-    .where('units', '>', 0)
-    .orderBy('uid')
-    .orderBy('item');
+export const getFunds = withSlonik<Fund[]>(async (db) => {
+  const { rows } = await db.query<{
+    uid: string;
+    name: string;
+    units: number;
+    cost: number;
+  }>(sql`
+  SELECT uid, item as name, units, cost
+  FROM (
+    SELECT ${sql.join(
+      [
+        sql`f.uid`,
+        sql`f.item`,
+        sql`ROUND(SUM(ft.units)::decimal, 5) AS units`,
+        sql`SUM(ft.cost)::float AS cost`,
+      ],
+      sql`, `,
+    )}
+    FROM funds f
+    INNER JOIN funds_transactions ft ON ft.fund_id = f.id
+    GROUP BY f.uid, f.item
+  ) r
+  WHERE r.units > 0
+  ORDER BY r.uid, r.item
+  `);
 
   return rows
-    .map(({ uid, item, units, cost }) => ({
+    .map(({ uid, name, units, cost }) => ({
       uid,
-      name: item,
-      hash: fundHash(item, config.data.funds.salt),
-      broker: getBroker(item),
-      units: Number(units.toFixed(5)),
+      name,
+      hash: fundHash(name, config.data.funds.salt),
+      broker: getBroker(name),
+      units,
       cost,
     }))
-    .map(fund => ({
+    .map((fund) => ({
       ...fund,
       url: getFundUrl(fund),
     }));
-}
+});
 
 export async function processScrape(flags: CLIOptions): Promise<void> {
   const { holdings, prices } = flags;
@@ -83,7 +80,7 @@ export async function processScrape(flags: CLIOptions): Promise<void> {
     }
 
     const groupedFunds = groupBy(funds, 'url');
-    const uniqueFunds = Object.keys(groupedFunds).map(url => ({
+    const uniqueFunds = Object.keys(groupedFunds).map((url) => ({
       ...groupedFunds[url][0],
       units: groupedFunds[url].reduce((last, { units: value }) => last + value, 0),
       cost: groupedFunds[url].reduce((last, { cost: value }) => last + value, 0),
