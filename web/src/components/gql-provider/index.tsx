99b3f6c2b479bde3620e9503baafbb9d192666f5
Fela Maslen <felamaslen@gmail.com>
2020-12-11 20:14:19 +0000

    fix: upgraded graphql-ws and added subscription retry logic

--- web/src/components/gql-provider/index.tsx
+++ web/src/components/gql-provider/index.tsx
@@ -1,53 +1,56 @@
 import { createClient as createWSClient } from 'graphql-ws';
-import React, { useContext, useMemo } from 'react';
-import { Provider, createClient, defaultExchanges, subscriptionExchange } from 'urql';
+import moize from 'moize';
+import React, { useContext } from 'react';
+import { Client, createClient, defaultExchanges, Provider, subscriptionExchange } from 'urql';
 
 import { Spinner } from '~client/components/spinner';
 import { ApiContext } from '~client/hooks';
 
 const isSecure = window.location.protocol === 'https:';
+const wsUrl = `${isSecure ? 'wss' : 'ws'}://${window.location.host}/graphql`;
 
 type LoggedInProps = { apiKey: string };
 
+const getClient = moize(
+  (apiKey: string): Client => {
+    const wsClient = createWSClient({
+      url: wsUrl,
+      retryAttempts: Infinity,
+      retryWait: (retries): Promise<void> =>
+        new Promise((resolve) => setTimeout(resolve, 1000 * 2 ** Math.min(retries, 4))),
+      connectionParams: {
+        apiKey,
+      },
+    });
+
+    return createClient({
+      url: '/graphql',
+      fetchOptions: {
+        headers: {
+          Authorization: apiKey,
+        },
+      },
+      exchanges: [
+        ...defaultExchanges,
+        subscriptionExchange({
+          forwardSubscription: (operation) => ({
+            subscribe: (sink): { unsubscribe: () => void } => {
+              const dispose = wsClient.subscribe(operation, sink);
+              return {
+                unsubscribe: dispose,
+              };
+            },
+          }),
+        }),
+      ],
+    });
+  },
+  { maxSize: 1 },
+);
+
 const LoggedInWrapper: React.FC = ({ children }) => {
   const apiKey = useContext(ApiContext);
-
-  const wsClient = useMemo(
-    () =>
-      createWSClient({
-        url: `${isSecure ? 'wss' : 'ws'}://${window.location.host}/graphql`,
-        connectionParams: {
-          apiKey,
-        },
-      }),
-    [apiKey],
-  );
-
-  const client = useMemo(
-    () =>
-      createClient({
-        url: '/graphql',
-        fetchOptions: {
-          headers: {
-            Authorization: apiKey,
-          },
-        },
-        exchanges: [
-          ...defaultExchanges,
-          subscriptionExchange({
-            forwardSubscription: (operation) => ({
-              subscribe: (sink): { unsubscribe: () => void } => {
-                const dispose = wsClient.subscribe(operation, sink);
-                return {
-                  unsubscribe: dispose,
-                };
-              },
-            }),
-          }),
-        ],
-      }),
-    [apiKey, wsClient],
-  );
+  const client = getClient(apiKey);
 
   if (!client) {
     return <Spinner cover />;
