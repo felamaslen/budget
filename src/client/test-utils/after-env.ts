2bf143c829af1d64a163acd2f7640f6f591b656d
Fela Maslen <felamaslen@gmail.com>
2021-10-19 22:57:37 +0100

    feat: image snapshot dependencies

--- src/client/test-utils/after-env.ts
+++ src/client/test-utils/after-env.ts
@@ -1,8 +1,11 @@
 import '@testing-library/jest-dom';
 import jestFetchMock from 'jest-fetch-mock';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import nock from 'nock';
 
 beforeAll(() => {
+  expect.extend({ toMatchImageSnapshot });
+
   jestFetchMock.enableMocks();
   nock.disableNetConnect();
   nock.enableNetConnect('127.0.0.1');
