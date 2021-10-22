e5c81113d9e725463f44b8ebc41f74710bdc8846
Fela Maslen <felamaslen@gmail.com>
2021-10-22 11:36:32 +0100

    test: visual regression test for app logo

--- src/client/components/app-logo/index.spec.tsx
+++ src/client/components/app-logo/index.spec.tsx
@@ -1,5 +1,6 @@
 import { render, RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { generateImage } from 'jsdom-screenshot';
 import React from 'react';
 import { AppLogo, Props } from '.';
 
@@ -11,11 +12,11 @@ describe('<AppLogo />', () => {
 
   const setup = (customProps = {}): RenderResult => render(<AppLogo {...props} {...customProps} />);
 
-  it('should render a logo', () => {
-    // TODO: convert this to an image test
+  it('should render a logo', async () => {
     expect.assertions(1);
-    const { getByText } = setup();
-    expect(getByText('Budget')).toBeInTheDocument();
+    setup();
+    const screenshot = await generateImage();
+    expect(screenshot).toMatchImageSnapshot();
   });
 
   it('should render a settings link', () => {
