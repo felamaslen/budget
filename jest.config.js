256c673aadbdc6b6af66c5b59e9c75e6cbccaf02
Fela Maslen <felamaslen@gmail.com>
2021-10-25 16:56:34 +0100

    chore: removed ts-jest

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,6 @@ const tsconfig = require('./tsconfig.json');
 
 module.exports = {
   clearMocks: true,
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-      diagnostics: true,
-    },
-  },
   moduleNameMapper: mapPaths(tsconfig),
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   collectCoverage: process.env.COVERAGE === 'true' || ['true', '1'].includes(process.env.CI),
