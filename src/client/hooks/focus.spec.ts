c90ac0e3dd9ac7f8307753c05c05eac458d0a65e
Fela Maslen <felamaslen@gmail.com>
2021-10-31 18:08:17 +0000

    test: focus hook

---
+++ src/client/hooks/focus.spec.ts
@@ -0,0 +1,37 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useWindowFocus } from './focus';
+
+describe(useWindowFocus.name, () => {
+  const onFocus = jest.fn();
+
+  it('should fire onFocus when the browser regains focus, with the time since last focus', () => {
+    expect.assertions(4);
+    jest.useFakeTimers();
+
+    renderHook(() => useWindowFocus(onFocus));
+
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onFocus).toHaveBeenCalledWith(0);
+
+    act(() => {
+      Object.defineProperty(document, 'hidden', {
+        configurable: true,
+        get: () => true,
+      });
+      document.dispatchEvent(new Event('visibilitychange'));
+    });
+
+    jest.advanceTimersByTime(1562);
+
+    act(() => {
+      Object.defineProperty(document, 'hidden', {
+        configurable: true,
+        get: () => false,
+      });
+      document.dispatchEvent(new Event('visibilitychange'));
+    });
+
+    expect(onFocus).toHaveBeenCalledTimes(2);
+    expect(onFocus).toHaveBeenNthCalledWith(2, 1562);
+  });
+});
