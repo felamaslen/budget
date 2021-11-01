535eb9c186da859747fda873619f3063cfab7aaf
Fela Maslen <felamaslen@gmail.com>
2021-11-01 16:48:51 +0000

    test: don't mock ioredis in integration tests

--- src/api/test-utils/after-env.unit.ts
+++ src/api/test-utils/after-env.unit.ts
@@ -1,5 +1,9 @@
 import nock from 'nock';
 
+const Redis = require('ioredis-mock/jest'); // eslint-disable-line
+
+jest.mock('ioredis', () => Redis);
+
 beforeAll(() => {
   nock.disableNetConnect();
   nock.enableNetConnect('127.0.0.1');
