adccf66a25526af93abd408e5a305c70af4a0849
Fela Maslen <felamaslen@gmail.com>
2021-11-03 13:18:22 +0000

    chore: removed dead code

--- src/client/components/planning/hooks/months.ts
+++ src/client/components/planning/hooks/months.ts
@@ -3,13 +3,9 @@ import endOfMonth from 'date-fns/endOfMonth';
 import getMonth from 'date-fns/getMonth';
 import getYear from 'date-fns/getYear';
 import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
 
 import type { PlanningMonth } from '../types';
-import { filterNetWorthByMonth } from './utils';
 
-import { getEntries } from '~client/selectors';
-import type { NetWorthEntryNative } from '~client/types';
 import {
   getDateFromYearAndMonth,
   getFinancialYearFromYearMonth,
@@ -31,9 +27,4 @@ export function usePlanningMonths(year: number): PlanningMonth[] {
     const atYear = getDateFromYearAndMonth(year, startMonth);
     return fillMonths(atYear, 12);
   }, [year]);
-}
-
-export function useRecordedMonthNetWorth(date: Date): NetWorthEntryNative | undefined {
-  const entries = useSelector(getEntries);
-  return filterNetWorthByMonth(entries, date);
 }
