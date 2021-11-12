736ded73801d3712f6559d9aa7ce38073cd1562f
Fela Maslen <felamaslen@gmail.com>
2021-11-12 09:59:09 +0000

    refactor: typescript storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,17 @@
-const path = require('path');
+import path from 'path';
+import { Configuration } from 'webpack';
 
-const toPath = (_path) => path.join(process.cwd(), _path);
+const toPath = (_path: string) => path.join(process.cwd(), _path);
 
-module.exports = {
+export default {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config) => ({
+  webpackFinal: async (config: Configuration): Promise<Configuration> => ({
     ...config,
     resolve: {
       ...config.resolve,
       alias: {
-        ...config.resolve.alias,
+        ...config.resolve?.alias,
         '@emotion/core': toPath('node_modules/@emotion/react'),
         '@emotion/styled': toPath('node_modules/@emotion/styled'),
         'emotion-theming': toPath('node_modules/@emotion/react'),
