c84b3ac95d097d8c9e0a910498d9d5bdc84084f5
Fela Maslen <felamaslen@gmail.com>
2021-12-02 17:21:30 +0000

    test: mock worker in all tests

--- src/client/test-utils/after-env.ts
+++ src/client/test-utils/after-env.ts
@@ -2,6 +2,24 @@ import '@testing-library/jest-dom';
 import jestFetchMock from 'jest-fetch-mock';
 import nock from 'nock';
 
+jest.mock('worker-loader!../../../workers/prices', () => {
+  class MyMockWorker {
+    onmessage: (_: unknown) => void = () => {
+      /* pass */
+    };
+
+    postMessage(data: { type: 'start' | 'stop' }): void {
+      // eslint-disable-line
+      if (data.type === 'start') {
+        setTimeout(() => {
+          this.onmessage('Fetch!');
+        }, 5);
+      }
+    }
+  }
+  return MyMockWorker;
+});
+
 beforeAll(() => {
   jestFetchMock.enableMocks();
   nock.disableNetConnect();
